9d45a75914f01c121f9b53e1e440fecbf7670587
Willian B <willian_brener@hotmail.com>
2021-02-28 18:17:26 -0300

    qwe

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const cors =  require('cors');
 
 const app = express();
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/marvelflix'));
